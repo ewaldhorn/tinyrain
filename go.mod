05a7b200c012db974fcc11ff672f9616abc01268
Ewald Horn <ewaldhorn@gmail.com>
2024-12-31 11:36:36 +0200

    Basic build working

---
+++ go.mod
@@ -0,0 +1,3 @@
+module tinyrain
+
+go 1.23.4
--- src/main.go
+++ src/main.go
@@ -1,3 +1,11 @@
 package main
 
-func main() {}
+import "tinyrain/src/dom"
+
+func main() {
+	dom.Hide("loading")
+
+	// prevent the app for closing - it stays running for the life of the webpage
+	ch := make(chan struct{})
+	<-ch
+}
