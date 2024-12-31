e5f526c8304188520f9d9be468f155459ef78995
Ewald Horn <ewaldhorn@gmail.com>
2024-12-31 11:43:25 +0200

    Adding a way to console log

--- src/main.go
+++ src/main.go
@@ -3,6 +3,7 @@ package main
 import "tinyrain/src/dom"
 
 func main() {
+	dom.ConsoleLog("Starting...")
 	dom.Hide("loading")
 
 	// prevent the app for closing - it stays running for the life of the webpage
